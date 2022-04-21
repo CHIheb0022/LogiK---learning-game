b2e641d3b83193402aab901937bd3a0bc2981afa
ismailbouhlel611 <ismail.bouhlel@ensi-uma.tn>
2022-04-21 21:58:10 +0100

    overlay added

--- lib/MainMenu.dart
+++ lib/MainMenu.dart
@@ -1,9 +1,122 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/matchesGame.dart';
-
+import 'dart:ui' as ui;
 class MainMenuPage extends StatelessWidget {
   const MainMenuPage({Key? key}) : super(key: key);
+OverlayEntry _getEntry(context) {
+    OverlayEntry entry = OverlayEntry(builder: (_) => Container());
 
+    entry = OverlayEntry(
+      opaque: false,
+      maintainState: true,
+      builder: (_) => Positioned(
+        left: 0,
+        bottom: 0,
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: BackdropFilter(
+          filter: ui.ImageFilter.blur(
+            sigmaX: 2,
+            sigmaY: 2,
+          ),
+          child: Material(
+            type: MaterialType.transparency,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      width: 350,
+                      height: 350,
+                      color: Colors.blue[700],
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(0, 0, 0, 20),
+                            child: SizedBox(
+                              width: 230,
+                              height: 50,
+                              child: OutlinedButton(
+                                  onPressed: () {
+                                    Navigator.pushAndRemoveUntil(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              const Matches(title: '',)),
+                                      (Route<dynamic> route) => false,
+                                    );
+                                    entry.remove();
+                                  },
+                                  child:  Text(
+                                    "Restart Level",
+                                    style: TextStyle(
+              fontSize: 20,
+              foreground: Paint()
+             ..shader = ui.Gradient.linear(
+              const Offset(0, 20),
+               const Offset(150, 20),
+               <Color>[
+               Colors.red,
+                Colors.yellow,
+        ],
+      )
+  ),
+                                  ),
+                                  style: OutlinedButton.styleFrom(
+                                      side: const BorderSide(
+                                          width: 3.0, color: Colors.white))),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(0, 0, 0, 20),
+                            child: SizedBox(
+                              width: 230,
+                              height: 50,
+                              child: OutlinedButton(
+                                  onPressed: () {
+                                    Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) => const MainMenuPage()));
+                                    entry.remove();
+                                  },
+                                  child:  Text(
+                                    "Main Menu",
+                                    style: TextStyle(
+              fontSize: 20,
+              foreground: Paint()
+             ..shader = ui.Gradient.linear(
+              const Offset(0, 20),
+               const Offset(150, 20),
+               <Color>[
+               Colors.red,
+                Colors.yellow,
+        ],
+      )
+  ),
+                                  ),
+                                  style: OutlinedButton.styleFrom(
+                                      side: const BorderSide(
+                                          width: 3.0, color: Colors.white))),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+    return entry;
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,14 +126,17 @@ class MainMenuPage extends StatelessWidget {
          Center(child: Column(children:<Widget> [Text("****************"),
           Text("****************"),
           Text("****************"),
-          ElevatedButton(onPressed: (){
-          Navigator.push(context, MaterialPageRoute(builder: (context)=> const Matches(title: '',)));}, 
-          child: Text('matches ')),]
-          ) 
+                     
+           ElevatedButton(onPressed: (){
+             Overlay.of(context)?.insert(_getEntry(context));
+           },
+            child: Text('Matches ')),
+          ]
           )
+          )
+          ]
+          )
+          );
 
-       ]
-        )
-        );
   }
 }
--- lib/matchesGame.dart
+++ lib/matchesGame.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:ui' as ui;
 
+import 'package:flutter_application_1/MainMenu.dart';
 
 class Matches extends StatefulWidget {
   const Matches({Key? key, required this.title}) : super(key: key);
@@ -14,11 +16,142 @@ class MatchesGame extends State<Matches> {
   int PlayerTurn = 0; 
   int NbrMatches=20;
   int CpuTurn =0;
+ OverlayEntry _getEntry(context) {
+    OverlayEntry entry = OverlayEntry(builder: (_) => Container());
 
+    entry = OverlayEntry(
+      opaque: false,
+      maintainState: true,
+      builder: (_) => Positioned(
+        left: 0,
+        bottom: 0,
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: BackdropFilter(
+          filter: ui.ImageFilter.blur(
+            sigmaX: 2,
+            sigmaY: 2,
+          ),
+          child: Material(
+            type: MaterialType.transparency,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      width: 350,
+                      height: 350,
+                      color: Colors.blue[700],
+                      child: Column(
+                        children: [
+                           Padding(
+                            padding: const EdgeInsets.fromLTRB(0, 10, 0, 100),
+                            child: Text(
+                              "You Lost !",
+                             style: TextStyle(
+              fontSize: 40,
+              foreground: Paint()
+             ..shader = ui.Gradient.linear(
+              const Offset(0, 20),
+               const Offset(150, 20),
+               <Color>[
+               Colors.red,
+                Colors.yellow,
+        ],
+      )
+  ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(0, 0, 0, 20),
+                            child: SizedBox(
+                              width: 230,
+                              height: 50,
+                              child: OutlinedButton(
+                                  onPressed: () {
+                                    Navigator.pushAndRemoveUntil(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              const Matches(title: '',)),
+                                      (Route<dynamic> route) => false,
+                                    );
+                                    entry.remove();
+                                  },
+                                  child:  Text(
+                                    "Restart Level",
+                                    style: TextStyle(
+              fontSize: 20,
+              foreground: Paint()
+             ..shader = ui.Gradient.linear(
+              const Offset(0, 20),
+               const Offset(150, 20),
+               <Color>[
+               Colors.red,
+                Colors.yellow,
+        ],
+      )
+  ),
+                                  ),
+                                  style: OutlinedButton.styleFrom(
+                                      side: const BorderSide(
+                                          width: 3.0, color: Colors.white))),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(0, 0, 0, 20),
+                            child: SizedBox(
+                              width: 230,
+                              height: 50,
+                              child: OutlinedButton(
+                                  onPressed: () {
+                                    Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) => const MainMenuPage()));
+                                    entry.remove();
+                                  },
+                                  child:  Text(
+                                    "Main Menu",
+                                    style: TextStyle(
+              fontSize: 20,
+              foreground: Paint()
+             ..shader = ui.Gradient.linear(
+              const Offset(0, 20),
+               const Offset(150, 20),
+               <Color>[
+               Colors.red,
+                Colors.yellow,
+        ],
+      )
+  ),
+                                  ),
+                                  style: OutlinedButton.styleFrom(
+                                      side: const BorderSide(
+                                          width: 3.0, color: Colors.white))),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+    return entry;
+  }
   void Game() {
     setState(() {
       NbrMatches=NbrMatches-PlayerTurn;
-      if(NbrMatches==1){;}
+      if(NbrMatches==1){Overlay.of(context)?.insert(_getEntry(context));}
       switch(NbrMatches % 4) { 
    case 3: { 
       CpuTurn= 2;
@@ -36,7 +169,7 @@ class MatchesGame extends State<Matches> {
    }   
 } 
 NbrMatches=NbrMatches-CpuTurn;
-if(NbrMatches==1){;}
+if(NbrMatches==1){Overlay.of(context)?.insert(_getEntry(context));}
     });
   }
 
@@ -57,13 +190,36 @@ Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'the number of matches is :',
-              style: TextStyle(color: Colors.cyan),
-            ),
+             Center(child: Text(
+              'The number of matches is',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+              fontSize: 40,
+              foreground: Paint()
+             ..shader = ui.Gradient.linear(
+              const Offset(0, 20),
+               const Offset(150, 20),
+               <Color>[
+               Colors.red,
+               Colors.yellow,
+        ],
+      )
+  ),
+            ),),
             Text(
               '$NbrMatches',
-              style: Theme.of(context).textTheme.headline4,
+              style: TextStyle(
+              fontSize: 40,
+              foreground: Paint()
+             ..shader = ui.Gradient.linear(
+              const Offset(0, 20),
+               const Offset(150, 20),
+               <Color>[
+               Colors.red,
+                Colors.yellow,
+        ],
+      )
+  ),
               
             ),
             Align(
